0e2d1db9156d6270f75c0d83e5a480d07b4a081f
etozhealexis <alexisworksya@yandex.ru>
2024-02-19 22:41:13 +0300

    initial

---
+++ src/main/java/ru/etozhealexis/algorithmcomplexitytheory/AlgorithmComplexityTheoryApplication.java
@@ -0,0 +1,13 @@
+package ru.etozhealexis.algorithmcomplexitytheory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmComplexityTheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmComplexityTheoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/etozhealexis/algorithmcomplexitytheory/AlgorithmComplexityTheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.etozhealexis.algorithmcomplexitytheory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmComplexityTheoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
